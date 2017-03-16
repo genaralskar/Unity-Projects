3328eef8c3be466da389e501eb1647b0a781f51d
Unknown <genaralskar1@gmail.com>
2017-03-16 16:19:24 -0600

    Add Increase Array Project

---
+++ Array Button Project/Assets/IncreaseArray.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseArray : MonoBehaviour {
+
+	//public int[] list;
+
+	public List<int> List = new List<int>();
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ArrayIncrease () {
+		//list = new int[list.Length + 1];
+		//list[list.Length - 1] = list.Length;
+
+		List.Add(List.Count);
+	}
+}
